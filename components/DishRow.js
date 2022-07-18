000c27d662d43474621161e363d27ebd0d2cc0b2
M Ali <ma_uk@hotmail.com>
2022-07-18 18:44:00 +0100

    redux implemented

--- components/DishRow.js
+++ components/DishRow.js
@@ -3,9 +3,27 @@ import React, { useState } from 'react'
 import Currency from "react-currency-formatter"
 import { urlFor } from '../sanity'
 import { MinusCircleIcon, PlusCircleIcon } from "react-native-heroicons/solid"
+import { useDispatch, useSelector } from 'react-redux'
+import { 
+    addToBasket,
+    removeFromBasket, 
+    selectBasketItems, 
+    selectBasketItemsWithId } from '../features/basketSlice'
 
 const DishRow = ({ id, name, description, price, image }) => {
     const [isPressed, setIsPressed] = useState(false)
+    const items = useSelector((state) => selectBasketItemsWithId(state, id))
+    const dispatch = useDispatch()
+
+    const addItemToBasket = () => {
+        dispatch(addToBasket({ id, name, description, price, image }))
+    }
+
+    const removeItemFromBasket = () => {
+        if(!items.length > 0) return
+
+        dispatch(removeFromBasket({ id }))
+    }
 
   return (
     <>
@@ -39,16 +57,18 @@ const DishRow = ({ id, name, description, price, image }) => {
         {isPressed && (
             <View className="bg-white px-4 py-1">
                 <View className="flex-row items-center space-x-2">
-                    <TouchableOpacity>
+                    <TouchableOpacity 
+                        disabled={!items.length}
+                        onPress={removeItemFromBasket}>
                         <MinusCircleIcon 
-                            color="#00CCBB"  
+                            color={items.length > 0 ? "#00CCBB" : 'gray'}  
                             size={40}
                         />
                     </TouchableOpacity>
 
-                    <Text>0</Text>
+                    <Text>{items.length}</Text>
 
-                    <TouchableOpacity>
+                    <TouchableOpacity onPress={addItemToBasket}>
                         <PlusCircleIcon 
                             color="#00CCBB"  
                             size={40}
